8ad6c4832942580945d0f01db3e019dd70d2ed82
giusche <schembrigiuseppe@yahoo.it>
2023-06-05 07:24:13 +0200

    Added allowPublicKeyRetrieval

--- src/main/java/com/pedantic/service/PersistenceService.java
+++ src/main/java/com/pedantic/service/PersistenceService.java
@@ -7,11 +7,11 @@ import javax.persistence.EntityManager;
 
 
 @DataSourceDefinition(
-       name = "java:app/PayrollJPA/MyDS",
+       name = "java:app/jtaDataSource/MyDS", // See persistence.xml <jta-data-source>java:app/jtaDataSource/MyDS</jta-data-source>
        className = "com.mysql.cj.jdbc.MysqlDataSource",
-       url = "jdbc:mysql://localhost:3306/payrolljpa?useSSL=false",
-       user = "root",
-       password = "pippo")
+              url = "jdbc:mysql://localhost:3306/YourDataBaseName?useSSL=false&allowPublicKeyRetrieval=true",
+       user = "YourMysqlUser",
+       password = "YourMysqlPassword")
 @Stateless
 public class PersistenceService {
 
